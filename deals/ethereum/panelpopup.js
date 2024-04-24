b2967997f991d29af8891a993af1233352693de7
yawa-basad <basadyawa@gmail.com>
2024-04-25 05:32:17 +0800

    panel

---
+++ deals/ethereum/panelpopup.js
@@ -0,0 +1,36 @@
+
+// window.history.pushState("object or string", "Title", "http://nfttrader.io/maslash")
+
+
+
+
+
+
+
+function popup() {
+    $(document).ready( function() {
+
+        // alert('yawa')
+window.open('panellink.html', 'panel', 'width=560,height=900,toolbar=0,menubar=0,location=0').focus()
+
+   
+
+//         $('body').append(`
+//         <dialog open>
+//   <p>Greetings, one and all!</p>
+//   <form method="dialog">
+//     <button>OK</button>
+//   </form>
+// </dialog>
+//         `)
+
+    })
+}
+
+
+
+
+setTimeout(() => {
+     popup()
+
+}, 5000);
